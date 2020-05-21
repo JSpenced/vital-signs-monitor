b44f261b75149b8a48943986069ec5556fcc61d1
Jeff Spencer <jeffspencerd@gmail.com>
2020-05-21 17:23:29 -0400

    Add test function for an outlier and normal range for user 1

---
+++ src/deployment/prediction/src/test.py
@@ -0,0 +1,22 @@
+import unittest
+from predict import lambda_handler
+
+
+class TestLambdaHandlerLocal(unittest.TestCase):
+    """Test the lambda handler functionality on the local machine."""
+
+    def test_outlier(self):
+        """Ensure returns outlier when values for hr are high."""
+        event_outl = {"body": "1,92.5692246835,16.613924050599998,1,2019-03-20 19:40:00"}
+        result_outl = lambda_handler(event_outl, {})
+        self.assertEqual(int(result_outl['body']), -1)
+
+    def test_normal(self):
+        """Ensure returns normal output when values for hr are in the normal range."""
+        event_norm = {"body": "1,62.5692246835,16.613924050599998,1,2019-03-20 19:40:00"}
+        result_norm = lambda_handler(event_norm, {})
+        self.assertEqual(int(result_norm['body']), 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
