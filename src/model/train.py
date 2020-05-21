d38ab3d866351dec86e592de9e12174d70ec54c5
Jeff Spencer <jeffspencerd@gmail.com>
2020-05-21 16:56:20 -0400

    Trains model for each user and saves to S3.
    Each user model is fit to a Gaussian after ignoring negative values,
    when the user is out of the room, and outliers.
    Each user's model is also saved to the S3 bucket.

---
+++ src/model/train.py
@@ -0,0 +1,28 @@
+import sys
+import pandas as pd
+from sklearn.covariance import EllipticEnvelope
+from sklearn.model_selection import train_test_split
+sys.path.append('../deployment/prediction/src/')
+from utils import upload_model_to_s3
+from config import MODEL_FILENAME, MODEL_EXTENSION, BUCKET_NAME
+
+
+df = pd.read_csv('../../data/raw/data_2020_05.csv', parse_dates=['ts'])
+user_ids = df.user_id.unique()
+
+min_hr = 1  # Values below this ignored
+min_rr = 3  # Values below this ignored
+for user in user_ids:
+    X = df[(df.in_room == True) &
+           (df.user_id == user) &
+           (df.hr > min_hr) &
+           (df.rr > min_rr)][['hr', 'rr']]
+
+    X_tr, X_va = train_test_split(X, test_size=0.2, shuffle=False)
+
+    # Fit Gaussian to data to detect outliers
+    el = EllipticEnvelope(contamination=0.12)
+    el.fit(X_tr)
+
+    savepath = '../../data/models/'
+    upload_model_to_s3(el, user, savepath)
